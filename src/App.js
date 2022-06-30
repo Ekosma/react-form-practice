7d3d070fba22f79978da38d39fbebbba5c536d33
Evelyn <evelyn.a.kosma@gmail.com>
2022-06-30 15:27:19 -0500

    third example completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import First from './components/First_Practice/form'
 import Second from './components/Second_Practice/form'
+import Third from './components/Third_Practice/Form'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <First/>
     <Second />
+    <Third />
     </>
   )
 }
